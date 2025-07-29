309a01c76b1a956a99f7a9b614f794d5528e2b37
Kalicz Péter <kaliczp@gmail.com>
2025-07-29 12:22:00 +0200

    Oszlopnevek fájlból

--- Readme.R
+++ Readme.R
@@ -3,11 +3,8 @@
 library(readxl)
 
 ## Beolvasás
-## A dátum, az nem kell, B-U - ig a boxplot alapanyagok, 3-58-ig
-rawkut <- as.data.frame(read_excel("boxplotok vs távolság.xlsx", range = "B3:U58", col_names = FALSE))
-
-## Oszlopnevek, excelből
-colnames(rawkut) <- LETTERS[2:(ncol(rawkut)+1)]
+## A dátum, az nem kell, B-U - ig a boxplot alapanyagok, 2-58-ig fejléccel
+rawkut <- as.data.frame(read_excel("boxplotok vs távolság.xlsx", range = "B2:U58"))
 
 ## Boxplot
 boxplot(rawkut)
