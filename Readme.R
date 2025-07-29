280948b277e08139741d37e930f3664c635db2f7
Kalicz Péter <kaliczp@gmail.com>
2025-07-29 12:15:12 +0200

    Nulla vonal

--- Readme.R
+++ Readme.R
@@ -6,5 +6,9 @@ library(readxl)
 ## A dátum, az nem kell, B-U - ig a boxplot alapanyagok, 3-58-ig
 rawkut <- as.data.frame(read_excel("boxplotok vs távolság.xlsx", range = "B3:U58", col_names = FALSE))
 
+## Oszlopnevek, excelből
+colnames(rawkut) <- LETTERS[2:ncol(rawkut)]
+
 ## Boxplot
 boxplot(rawkut)
+axis(2,0,tck=1)
