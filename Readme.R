78aab9fa70082c8205317c5f49529af80ca74777
Kalicz Péter <kaliczp@gmail.com>
2025-07-29 12:55:22 +0200

    Felirat kiszedése

--- Readme.R
+++ Readme.R
@@ -6,6 +6,9 @@ library(readxl)
 ## A dátum, az nem kell, B-U - ig a boxplot alapanyagok, 2-58-ig fejléccel
 rawkut <- as.data.frame(read_excel("boxplotok vs távolság.xlsx", range = "B2:U58"))
 
+## Felirat a tetejére
+rawnev <- as.data.frame(read_excel("boxplotok vs távolság.xlsx", range = "B1:U2"))
+
 ## Boxplot
 boxplot(rawkut)
 axis(2,0,tck=1)
